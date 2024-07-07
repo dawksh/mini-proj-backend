979a25fc896759d9256de5e9251d2e86eb050731
daksh <akshd@outlook.com>
2024-07-07 20:47:36 +0530

    feat: add basic queries

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
-import { createTable, sql } from "./utils/postgres";
+import { sql } from "./utils/postgres";
 import { randomUUID } from "crypto";
 
 config();
@@ -10,7 +10,38 @@ const app = express();
 
 app.use(express.json());
 
-
+app.post("/user", async (req, res) => {
+    const { name, walletAddress } = req.body;
+    const id = randomUUID()
+    const query = await sql`
+    insert into users values(${id}, ${name}, ${walletAddress});
+    `
+    return res.status(200).send({
+        query,
+        status: "OK"
+    })
+})
+app.post("/credential", async (req, res) => {
+    const { title, txHash, description, image, owner, creator } = req.body;
+    const id = randomUUID()
+    const query = await sql`
+    insert into users values(${id}, ${title}, ${txHash}, ${description}, ${image}, ${owner}, ${creator});
+    `
+    return res.status(200).send({
+        query,
+        status: "OK"
+    })
+})
+app.get("/user", async (req, res) => {
+    const { walletAddress } = req.query;
+    const query = await sql`
+    select * from users where walletAddress ILIKE ${walletAddress as string};
+    `
+    return res.status(200).send({
+        result: query,
+        status: "OK"
+    })
+})
 
 app.listen(PORT, async () => {
     console.log("Server Listening on PORT: ", PORT);
--- src/utils/postgres.ts
+++ src/utils/postgres.ts
@@ -4,7 +4,7 @@ import { config } from "dotenv"
 config()
 
 export const createTable = `
-create table users(id varchar primary key, name varchar, walletAddress varchar);
+create table users(id varchar primary key, name varchar, walletAddress varchar unique);
 
 create table credentials(id varchar primary key, title varchar, tx_hash varchar, description varchar, image varchar, owner varchar, creator varchar, FOREIGN KEY (owner) REFERENCES users(id), FOREIGN KEY (creator) REFERENCES users(id));
 `
